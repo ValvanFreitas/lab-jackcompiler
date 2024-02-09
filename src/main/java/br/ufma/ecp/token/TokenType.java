d2aa29de41168f3777e55d02ca130994e24bb240
Valvan <67364437+ValvanFreitas@users.noreply.github.com>
2024-02-09 18:30:38 -0300

    Update Symbols TokenType.java
    Nesse commit foi atualizada a lista de simbolos reconhecidos pelo compilador.

--- src/main/java/br/ufma/ecp/token/TokenType.java
+++ src/main/java/br/ufma/ecp/token/TokenType.java
@@ -7,40 +7,28 @@ public enum TokenType {
     PLUS,MINUS,
 
      // Literals.
-     NUMBER,
-     STRING,
-
-
-     IDENT,
-
- 
+    NUMBER,STRING,IDENT,COMMA,SEMICOLON,RBRACKET,LBRACKET,
+    RBRACE,LBRACE,RPAREN,LPAREN,EQ,LT,GT,NOT,OR,AND,
+    ASTERISK,DOT,SLASH,
      // keywords
-     METHOD,
-     WHILE,
-     IF,
-     CLASS,
-     CONSTRUCTOR,
+    WHILE, CLASS,CONSTRUCTOR,FUNCTION,
+    METHOD,FIELD,STATIC,VAR,INT,
+    CHAR,BOOLEAN,VOID,TRUE,FALSE,
+    NULL,THIS,LET,DO,IF,ELSE, RETURN,/**
+     *
+     */
+    EOF,ILLEGAL;
 
-     EOF,
-
-     ILLEGAL;
-
-     static public boolean isSymbol (char c) {
+    static public boolean isSymbol (char c) {
         String symbols = "{}()[].,;+-*/&|<>=~";
-        return symbols.indexOf(c) > -1;
-    }
-
+        return symbols.indexOf(c) > -1;}
 
     static public boolean isKeyword (TokenType type) {
         List<TokenType> keywords  = 
             List.of(
-                METHOD,
-                WHILE,
-                IF,
-                CLASS,
-                CONSTRUCTOR
-            );
-            return keywords.contains(type);
-    }
-
+                WHILE, CLASS,CONSTRUCTOR,FUNCTION,
+                METHOD,FIELD,STATIC,VAR,INT,
+                CHAR,BOOLEAN,VOID,TRUE,FALSE,
+                NULL,THIS,LET,DO,IF,ELSE,RETURN);
+            return keywords.contains(type);} }
 }
