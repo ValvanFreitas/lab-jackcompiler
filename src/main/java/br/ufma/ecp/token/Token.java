7d8ed6d33a75cca8b3daa562b01d7f91e4f68810
Valvan <67364437+ValvanFreitas@users.noreply.github.com>
2024-02-09 18:50:25 -0300

    Update Token.java
    Nesse commit há um atributo adicional "line", que armazena o número da linha em que o token se encontra. A linha onde o token foi encontrado é incluída na saída.

--- src/main/java/br/ufma/ecp/token/Token.java
+++ src/main/java/br/ufma/ecp/token/Token.java
@@ -3,16 +3,18 @@ public class Token {
 
     public final TokenType type;
     public final String lexeme;
+    public final int line;
 
-    public Token (TokenType type, String lexeme) {
+    public Token (TokenType type, String lexeme, int line) {
         this.type = type;
         this.lexeme = lexeme;
-    }
+        this.line = line;}
 
     public String toString() {
         var type = this.type.toString();
+        String valor = lexeme;
         if (type.equals("NUMBER"))
-            type =  "intConst";
+            type =  "intergerConstant";
 
         if (type.equals("STRING"))
             type =  "stringConst";
@@ -20,14 +22,19 @@ public class Token {
         if (type.equals("IDENT"))
             type =  "identifier";
 
-        if (TokenType.isSymbol(lexeme.charAt(0)))
+        if (TokenType.isSymbol(lexeme.charAt(0))) {
             type = "symbol";
-
+            //Os símbolos <, >, ", e & são impressos como &lt;  &gt;  &quot; e &amp; 
+            //Para não conflitar com o significado destes símbolos no XML
+            if (valor.equals(">")) {
+                valor = "&gt;" ;} 
+            else if (valor.equals("<")) {
+                valor = "&lt;" ;} 
+            else if (valor.equals("\"")) {
+                valor = "&quot;" ;} 
+            else if (valor.equals("&")) {
+                valor = "&amp;" ;}}             
         if (TokenType.isKeyword(this.type) )
             type = "keyword";
-    
 
-        return "<"+ type +">" + lexeme + "</"+ type + ">";
-    }
-    
-}
+        return "<"+line+":"+ type + "> " + valor  + " </" + type + ">";}}
