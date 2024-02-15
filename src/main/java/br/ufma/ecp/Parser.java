f8484aa7a9dd21f9035f755f43aad9f3f0bb143f
Valvan <67364437+ValvanFreitas@users.noreply.github.com>
2024-02-15 08:58:51 -0300

    Update Parser.java
    Inclusão dos métodos parseDo(), parseSubroutineCall(), parseExpressionList(), parseExpression(), parseTerm().
    
    Inclusão de saída XML com os métodos XMLOutput() e printNoTerminal().

--- src/main/java/br/ufma/ecp/Parser.java
+++ src/main/java/br/ufma/ecp/Parser.java
@@ -1,64 +1,157 @@
 package br.ufma.ecp;
-
 import br.ufma.ecp.token.Token;
 import br.ufma.ecp.token.TokenType;
+import static br.ufma.ecp.token.TokenType.*;
 
 public class Parser {
-    private Scanner scan;
-    private Token currentToken;
+
+    private static class ParseError extends RuntimeException {}
+ 
+     private Scanner scan;
+     private Token currentToken;
+     private Token peekToken;
+     private StringBuilder xmlOutput = new StringBuilder();
+ 
+     public Parser(byte[] input) {
+         scan = new Scanner(input);
+         nextToken();}
+ 
+     private void nextToken() {
+         currentToken = peekToken;
+         peekToken = scan.nextToken();}
+ 
+ 
+     public void parse () {}
+   
+     // 'do' subrotineCall ';'
+     public void parseDo() {
+        printNonTerminal("doStatement");
+        expectPeek(DO);
+        expectPeek(IDENT);
+        parseSubroutineCall();
+        expectPeek(SEMICOLON);
+        printNonTerminal("/doStatement");}
+
+    // subroutineCall -> subroutineName '(' expressionList ')' | (className|varName)
+    // '.' subroutineName '(' expressionList ')
+    void parseSubroutineCall() {
+        if (peekTokenIs(LPAREN)) {
+            expectPeek(LPAREN);
+            parseExpressionList();
+            expectPeek(RPAREN);} 
+        else {
+            expectPeek(DOT);
+            expectPeek(IDENT);
+            expectPeek(LPAREN);
+            parseExpressionList();
+            expectPeek(RPAREN);}}
+
+    void parseExpressionList() {
+        printNonTerminal("expressionList");
+
+        if (!peekTokenIs(RPAREN)) // verifica se tem pelo menos uma expressao
+        {
+            parseExpression();}
+
+        // procurando as demais
+        while (peekTokenIs(COMMA)) {
+            expectPeek(COMMA);
+            parseExpression();}
+
+        printNonTerminal("/expressionList");}    
+
+     //letStatement -> 'let' identifier( '[' expression ']' )? '=' expression ';’
+     void parseLet() {
+        printNonTerminal("letStatement");
+        expectPeek(LET);
+        expectPeek(IDENT);
+
+        if (peekTokenIs(LBRACKET)) {
+            expectPeek(LBRACKET);
+            parseExpression();
+            expectPeek(RBRACKET);}
+
+        expectPeek(EQ);
+        parseExpression();
+        expectPeek(SEMICOLON);
+        printNonTerminal("/letStatement");}
+
+     void parseExpression() {
+        printNonTerminal("expression");
+        parseTerm ();
+        while (isOperator(peekToken.lexeme)) {
+            expectPeek(peekToken.type);
+            parseTerm();}
+        printNonTerminal("/expression");}
+
+     void parseTerm() {
+        printNonTerminal("term");
+        switch (peekToken.type) {
+          case NUMBER:
+            expectPeek(TokenType.NUMBER);
+            break;
+          case STRING:
+            expectPeek(TokenType.STRING);
+            break;
+          case FALSE:
+          case NULL:
+          case TRUE:
+            expectPeek(TokenType.FALSE, TokenType.NULL, TokenType.TRUE);
+            break;
+          case THIS:
+            expectPeek(TokenType.THIS);
+            break;
+          case IDENT:
+            expectPeek(TokenType.IDENT);
+            break;
+          default:
+            throw error(peekToken, "term expected");}
     
-    public Parser (byte[] input) {
-        scan = new Scanner(input);
-        currentToken = scan.nextToken();
-        
-    }
+        printNonTerminal("/term");}
+ 
+     // funções auxiliares
 
-    public void parse () {
-        expr();
-    }
+     static public boolean isOperator(String op) {
+        return op!= "" && "+-*/<>=~&|".contains(op);}
 
-    void expr() {
-        number();
-        oper();
-    }
-
-    void number () {
-        System.out.println(currentToken.lexeme);
-        match(TokenType.NUMBER);
-    }
-
-    private void nextToken () {
-        currentToken = scan.nextToken();
-    }
-
-   private void match(TokenType t) {
-        if (currentToken.type == t) {
-            nextToken();
-        }else {
-            throw new Error("syntax error");
-        }
-   }
-
-    void oper () {
-        if (currentToken.type == TokenType.PLUS) {
-            match(TokenType.PLUS);
-            number();
-            System.out.println("add");
-            oper();
-        } else if (currentToken.type == TokenType.MINUS) {
-            match(TokenType.MINUS);
-            number();
-            System.out.println("sub");
-            oper();
-        } else if (currentToken.type == TokenType.EOF) {
-            // vazio
-        } else {
-            throw new Error("syntax error");
-        }
-    }
-
-    public String VMOutput() {
-        return "";
-    }
-
-}
+     public String XMLOutput() {
+         return xmlOutput.toString();}
+ 
+     private void printNonTerminal(String nterminal) {
+         xmlOutput.append(String.format("<%s>\r\n", nterminal));}
+ 
+ 
+     boolean peekTokenIs(TokenType type) {
+         return peekToken.type == type;}
+ 
+     boolean currentTokenIs(TokenType type) {
+         return currentToken.type == type;}
+ 
+     private void expectPeek(TokenType... types) {  //se for o token esperado, ele avança e imprime esse token
+         for (TokenType type : types) {
+             if (peekToken.type == type) {
+                 expectPeek(type);
+                 return;}}
+ 
+        throw error(peekToken, "Expected a statement");}
+ 
+     private void expectPeek(TokenType type) {
+         if (peekToken.type == type) {
+             nextToken();
+             xmlOutput.append(String.format("%s\r\n", currentToken.toString()));
+         } else {
+             throw error(peekToken, "Expected "+type.name());}}
+ 
+ 
+     private static void report(int line, String where,
+         String message) {
+             System.err.println(
+             "[line " + line + "] Error" + where + ": " + message);}
+ 
+ 
+     private ParseError error(Token token, String message) {
+         if (token.type == TokenType.EOF) {
+             report(token.line, " at end", message);} 
+         else {
+             report(token.line, " at '" + token.lexeme + "'", message);}
+         return new ParseError();}}
