e247c487ba029974e70e0663600e4a86f170a261
Valvan <67364437+ValvanFreitas@users.noreply.github.com>
2024-02-20 10:10:11 -0300

    Update testSimpleExpression 1 e 2 CodeGenerator.java
    Adição de testes simples

--- src/test/java/br/ufma/ecp/CodeGenerator.java
+++ src/test/java/br/ufma/ecp/CodeGenerator.java
@@ -21,3 +21,37 @@ public class CodeGenerator extends TestSupport {
                 push constant 10       
                     """;
             assertEquals(expected, actual);}}
+
+    @Test
+    public void testSimpleExpression () {
+        var input = """
+            10 + 30
+            """;
+        
+        var parser = new Parser(input.getBytes(StandardCharsets.UTF_8));
+        parser.parseExpression();
+        String actual = parser.VMOutput();
+        String expected = """
+                push constant 10
+                push constant 30
+                add       
+                    """;
+            assertEquals(expected, actual);}
+
+    @Test
+    public void testSimpleExpression2 () {
+        var input = """
+            10 + 30 + 40
+            """;
+        
+        var parser = new Parser(input.getBytes(StandardCharsets.UTF_8));
+        parser.parseExpression();
+        String actual = parser.VMOutput();
+        String expected = """
+                push constant 10
+                push constant 30
+                add
+                push constant 40
+                add
+                    """;
+            assertEquals(expected, actual);}
