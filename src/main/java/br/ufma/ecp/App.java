d881a41cd10ffe5fd4e41b45bdc09283bdbbda65
Valvan <67364437+ValvanFreitas@users.noreply.github.com>
2024-02-21 19:08:59 -0300

    Update saveToFile App.java
    Inclusão do System.out.println("saved:"+fileName); na função saveToFile.

--- src/main/java/br/ufma/ecp/App.java
+++ src/main/java/br/ufma/ecp/App.java
@@ -11,14 +11,13 @@ import br.ufma.ecp.token.Token;
 public class App 
 {    
     public static void saveToFile(String fileName, String output) {
-         
         FileOutputStream outputStream;
         try {
             outputStream = new FileOutputStream(fileName);
             byte[] strToBytes = output.getBytes();
-            outputStream.write(strToBytes);
-    
-            outputStream.close();} 
+            outputStream.write(strToBytes);            
+            outputStream.close(); 
+            System.out.println("saved:"+fileName);}
         catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();} 
@@ -27,7 +26,6 @@ public class App
             e.printStackTrace();}}
 
     private static String fromFile(File file) {        
-
         byte[] bytes;
         try {
             bytes = Files.readAllBytes(file.toPath());
@@ -41,29 +39,23 @@ public class App
         if (args.length != 1) {
             System.err.println("Please provide a single file path argument.");
             System.exit(1);}
-
         File file = new File(args[0]);
-
         if (!file.exists()) {
             System.err.println("The file doesn't exist.");
             System.exit(1);}
-
         // we need to compile every file in the directory
         if (file.isDirectory()) {
             for (File f : file.listFiles()) {
                 if (f.isFile() && f.getName().endsWith(".jack")) {
-
                     var inputFileName = f.getAbsolutePath();
                     var pos = inputFileName.lastIndexOf('.');
                     var outputFileName = inputFileName.substring(0, pos) + ".vm";
-                    
                     System.out.println("compiling " +  inputFileName);
                     var input = fromFile(f);
                     var parser = new Parser(input.getBytes(StandardCharsets.UTF_8));
                     parser.parse();
                     var result = parser.VMOutput();
                     saveToFile(outputFileName, result);}}}
-
         // we only compile the single file         
         else if (file.isFile()) {
             if (!file.getName().endsWith(".jack"))  {
